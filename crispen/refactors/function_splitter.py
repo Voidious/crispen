823ba5123d2e0136ca0bec49ec3a0eb95d609a86
Patrick Cupka <pcupka@gmail.com>
2026-02-22 23:20:21 -0500

    crispen changes since initial commit

--- crispen/refactors/function_splitter.py
+++ crispen/refactors/function_splitter.py
@@ -778,6 +778,9 @@ class FunctionSplitter(Refactor):
 
     def _analyze(self, source: str) -> None:
         """Iteratively split oversized functions until stable or limit reached."""
+        return self._split_large_functions(source)
+
+    def _split_large_functions(self, source):
         current = source
 
         for _iteration in range(_MAX_SPLIT_ITERATIONS):
